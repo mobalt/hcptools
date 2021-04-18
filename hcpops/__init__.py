e4f0e2d5b3abd69f2168a47d5452819673b7ef29
Moises Baltazar <me@moisesb.com>
2021-04-18 10:41:48 -0500

    Add updatedb function to hcpops

--- hcpops/__init__.py
+++ hcpops/__init__.py
@@ -1,2 +1,17 @@
 from .hcpldap import HcpLdap, UsernameNotFound
 from .email import send_email
+
+
+def updatedb(session, firstname, lastname, email, status, username):
+    n_to_status = {0: "No Account", 1: "No DUT", 3: "Access Granted"}
+    status = n_to_status[status]
+
+    data = dict(
+        firstname=firstname,
+        lastname=lastname,
+        email=email,
+        status=status,
+        username=username,
+    )
+
+    return session.post("https://hcp-ops.humanconnectome.org/update_db", json=data)
